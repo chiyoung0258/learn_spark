9d7f91ef8c6e16d1b0e51b59daaa1a9addf6aa35
chiyoung <chiyoung0258@gmail.com>
2017-05-15 20:34:11 +0900

    update

--- src/main/scala/SparkSessionWordCount.scala
+++ src/main/scala/SparkSessionWordCount.scala
@@ -10,15 +10,17 @@ object SparkSessionWordCount {
 
   def main(args : Array[String]) : Unit = {
     val input = args(0)
+    val output = args(1)
 
-    val spark : SparkSession = SparkSession.builder.master("local").appName("wordcount2").getOrCreate()
-
+    val spark : SparkSession = SparkSession.builder.appName("wordcount2").getOrCreate()
+    
     import spark.implicits._
 
     val source = spark.read.text(input).as[String]
     val word = source.flatMap(line => line.split(" "))
     val groupedWord = word.groupByKey(_.toLowerCase())
-    val cnt = groupedWord.count().toDF().orderBy($"count(1)".desc)
-    cnt.show()
+//    val cnt = groupedWord.count().toDF().orderBy($"count(1)".desc)
+    val cnt = groupedWord.count()
+    cnt.rdd.saveAsTextFile(output)
   }
 }
